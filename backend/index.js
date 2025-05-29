470795dcc7298b1d6b188b37da4d103bf466cdfe
choppyback <seth.tay2812@gmail.com>
2025-05-29 09:17:41 +0800

    feat: update backend environment config and server setup
    - Added backend/.env file for jwtSecret
    - Modified backend/index.js to use environment variables and improved server setup

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ app.use(express.json());
 
 //routes
 
+app.use("/dashboard", require("./routes/dashboard"));
+
+app.use("/auth", require("./routes/auth"));
+
 app.listen(2000, () => {
   console.log(`Server is listening on port 2000`);
 });
