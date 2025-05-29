f3d93751e1f31fac5d814f880dc61c2e8c293c16
choppyback <seth.tay2812@gmail.com>
2025-05-29 09:08:08 +0800

    feat: add database schema

---
+++ backend/database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE tutorsaurus;
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL CHECK (role IN ('student', 'tutor', 'admin')),
+    faculty TEXT,
+    gender TEXT,
+    year_of_study INTEGER
+);
