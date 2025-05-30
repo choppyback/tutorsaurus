37b3312ce7d10e98303bc02d6ec613ce3e3d481a
choppyback <seth.tay2812@gmail.com>
2025-05-30 12:17:06 +0800

    chore: remove unnecessary comments from code

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -8,13 +8,6 @@ router.get("/", authorize, async (req, res) => {
       req.user,
     ]);
 
-    //if would be req.user if you change your payload to this:
-
-    //   function jwtGenerator(user_id) {
-    //   const payload = {
-    //     user: user_id
-    //   };
-
     res.json(user.rows[0]);
   } catch (err) {
     console.error(err.message);
