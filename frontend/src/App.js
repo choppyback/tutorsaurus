0a549e62eb43be1006454cbf1cdeb826e2f94b4b
choppyback <seth.tay2812@gmail.com>
2025-05-30 11:34:04 +0800

    feat: update App.js
    - Updated App.js to include and route to the new Landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Signup from "./Signup";
+import Landing from "./Landing";
 
 function App() {
   return (
-    <div className="App">
-      <Signup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
