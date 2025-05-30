2bc0d209df3cfd1912f64da0912bc466a8b12467
choppyback <seth.tay2812@gmail.com>
2025-05-30 21:15:32 +0800

    refactor: remove Login import from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 import Landing from "./Landing";
 import Login from "./Login";
