4c324de30dd6a535285c83a4cedb305b47cd4808
choppyback <seth.tay2812@gmail.com>
2025-06-08 22:57:21 +0800

    fix: improve input validation in validInfo

--- backend/middleware/validInfo.js
+++ backend/middleware/validInfo.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-  const { name, email, password, role } = req.body;
+  const { email, password } = req.body;
 
   function validEmail(userEmail) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
@@ -7,7 +7,7 @@ module.exports = function (req, res, next) {
 
   if (req.path === "/signup") {
     console.log(!email.length);
-    if (![name, email, password, role].every(Boolean)) {
+    if (![email, password].every(Boolean)) {
       return res.status(401).json("Missing Credentials");
     } else if (!validEmail(email)) {
       return res.status(401).json("Invalid Email");
