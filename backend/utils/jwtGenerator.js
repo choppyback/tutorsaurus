54c10980bfb0bec5bd37aaa644f6a525acd8a76e
choppyback <seth.tay2812@gmail.com>
2025-06-23 20:56:47 +0800

    chore: add missing comma in jwtGenerator.js

--- backend/utils/jwtGenerator.js
+++ backend/utils/jwtGenerator.js
@@ -2,11 +2,11 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 function jwtGenerator(user_id) {
-    const payload = {
-        user: user_id
-    };
+  const payload = {
+    user: user_id,
+  };
 
-    return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1h" });
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1h" });
 }
 
 module.exports = jwtGenerator;
