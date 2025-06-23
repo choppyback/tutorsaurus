84528ffab7ef7ac0d7a6def7a124e7d9d4387001
choppyback <seth.tay2812@gmail.com>
2025-06-23 20:50:27 +0800

    refactor: switch to Authorization header with Bearer token in authorize middleware

--- backend/middleware/authorize.js
+++ backend/middleware/authorize.js
@@ -3,21 +3,20 @@ require("dotenv").config();
 
 module.exports = async (req, res, next) => {
   try {
-    // Get token from header
-    const jwtToken = req.header("token");
+    const authHeader = req.header("Authorization");
 
-    // Check if not token
-    if (!jwtToken) {
-      return res.status(403).json("Not Authorized");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(403).json({ message: "Not Authorized" });
     }
 
-    //it is going to give use the user id (user:{id: user.id})
-    const payload = jwt.verify(jwtToken, process.env.jwtSecret);
+    const token = authHeader.split(" ")[1];
+
+    const payload = jwt.verify(token, process.env.jwtSecret);
 
     req.user = payload.user;
     next();
   } catch (err) {
     console.error(err.message);
-    return res.status(403).json("Not Authorized");
+    return res.status(403).json({ message: "Not Authorized" });
   }
 };
