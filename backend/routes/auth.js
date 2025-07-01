a8d815cf0f26891af198920cd86dc55a31e414ef
choppyback <seth.tay2812@gmail.com>
2025-07-01 13:13:31 +0800

    refactor: move login logic to loginController

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,11 @@ const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
 const validInfo = require("../middleware/validInfo");
 const authorize = require("../middleware/authorize");
+
+// Import Controllers
 const signupUser = require("../controllers/signupController");
+const loginUser = require("../controllers/loginController");
+
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
@@ -26,35 +30,7 @@ const upload = multer({ storage });
 router.post("/signup", upload.single("profile_pic"), signupUser);
 
 // Login route
-router.post("/login", validInfo, async (req, res) => {
-  try {
-    // 1. destructure the req.body
-    const { email, password } = req.body;
-
-    // 2. check if user doesn't exist (if not throw error)
-    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
-
-    if (user.rows.length === 0) {
-      return res.status(401).json("Password or Email is incorrect");
-    }
-
-    // 3. check is incoming password is the same as database password
-    const validPassword = await bcrypt.compare(password, user.rows[0].password);
-
-    if (!validPassword) {
-      return res.status(401).json("Password or Email is incorrect");
-    }
-
-    // 4. give them the jwt token
-    const jwtToken = jwtGenerator(user.rows[0].user_id);
-    res.json({ jwtToken });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
-});
+router.post("/login", validInfo, loginUser);
 
 // verify jwt token
 router.get("/verify", authorize, async (req, res) => {
