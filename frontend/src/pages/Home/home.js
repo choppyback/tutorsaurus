62e31b76ca34ba3b18c0c43c0ef39f7dcdf09c15
choppyback <seth.tay2812@gmail.com>
2025-07-07 20:36:48 +0800

    style: define styles for home page

--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -26,6 +26,33 @@ const styles = {
     pb: 3,
     height: "fit-content",
   },
+  card: {
+    display: "flex",
+    gap: 3,
+    alignItems: "center",
+    borderRadius: 3,
+    p: 3,
+    bgcolor: "#fff",
+    boxShadow: "0 2px 8px rgba(0,0,0,0.05)",
+  },
+  tutorImage: {
+    width: 163,
+    height: 217,
+    borderRadius: 2,
+    objectFit: "cover",
+  },
+  divider: {
+    width: "2px",
+    height: "100%",
+    bgcolor: "#eee",
+  },
+  bookButton: {
+    backgroundColor: "#A2CB75",
+    color: "#294A29",
+    "&:hover": {
+      backgroundColor: "#95bd68",
+    },
+  },
 };
 
 export default styles;
