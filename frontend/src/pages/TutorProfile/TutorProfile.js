63aac35ccf34b97184b8dcd7b4f2b492fc94f43e
choppyback <seth.tay2812@gmail.com>
2025-07-08 15:03:39 +0800

    feat: add custom styles for TutorProfile component

---
+++ frontend/src/pages/TutorProfile/TutorProfile.js
@@ -0,0 +1,18 @@
+const styles = {
+  tutorImage: {
+    width: 163,
+    height: 217,
+    borderRadius: 2,
+    objectFit: "cover",
+  },
+  divider: { m: 2 },
+  bookButton: {
+    backgroundColor: "#A2CB75",
+    color: "#294A29",
+    "&:hover": {
+      backgroundColor: "#95bd68",
+    },
+  },
+};
+
+export default styles;
