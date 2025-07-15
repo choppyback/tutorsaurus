bae657c537b79321e9fa8fbc717544f884e77bd9
choppyback <seth.tay2812@gmail.com>
2025-07-15 19:02:29 +0800

    feat: add /student route to bookings router

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -6,9 +6,11 @@ const authorize = require("../middleware/authorize");
 // Import Controller
 const createBooking = require("../controllers/bookingController");
 const tutorBookings = require("../controllers/tutorBookingsController");
+const studentBookings = require("../controllers/studentBookingsController");
 
 // Creating booking
 router.post("/", authorize, createBooking);
+router.get("/student", authorize, studentBookings);
 router.get("/:id", tutorBookings);
 
 module.exports = router;
