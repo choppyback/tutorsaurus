d107d3942121d4c50146af7173912e40d800ec1e
Jackson Chia <chiadong07@gmail.com>
2025-07-21 02:00:04 +0800

    fix: add Home route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
 
         <Route
           path="/admin"
