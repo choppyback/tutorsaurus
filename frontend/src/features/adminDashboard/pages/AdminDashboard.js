58e728224d191803607eebbd26da099128f4d9c7
choppyback <seth.tay2812@gmail.com>
2025-07-21 11:42:27 +0800

    style: add styles for AdminDashboard layout

---
+++ frontend/src/features/adminDashboard/pages/AdminDashboard.js
@@ -0,0 +1,10 @@
+const styles = {
+  page: {
+    background: "linear-gradient(277deg ,rgb(242, 255, 240), #fefae9)",
+    minHeight: "100vh",
+    pt: 4,
+    px: 3,
+  },
+};
+
+export default styles;
