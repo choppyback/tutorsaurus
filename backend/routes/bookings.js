40042bf9ab34f0fdb4e5823149bb706830a6827c
choppyback <seth.tay2812@gmail.com>
2025-07-21 21:32:57 +0800

    feat: add PATCH route for booking cancellation and import cancel controller

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -7,10 +7,13 @@ const authenticate = require("../middleware/authenticate");
 const createBooking = require("../controllers/bookingController");
 const tutorBookings = require("../controllers/tutorBookingsController");
 const studentBookings = require("../controllers/studentBookingsController");
+const cancelBooking = require("../controllers/cancelBookingController");
 
 // Creating booking
 router.post("/", authenticate, createBooking);
 router.get("/student", authenticate, studentBookings);
 router.get("/:id", tutorBookings);
+router.patch("/:id/cancel", authenticate, cancelBooking);
+//router.delete("/:id", authenticate, deleteBooking);
 
 module.exports = router;
