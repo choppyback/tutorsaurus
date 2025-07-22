c47ee1f4db965700330007b628ece930455aaac8
choppyback <seth.tay2812@gmail.com>
2025-07-22 20:36:09 +0800

    refactor: return response data from cancelBooking API call

--- frontend/src/features/viewBookings/api/cancelBooking.js
+++ frontend/src/features/viewBookings/api/cancelBooking.js
@@ -4,11 +4,13 @@ import BASE_URL from "../../../config/api";
 // change booking status to cancelled
 
 export const cancelBooking = async (bookingId, token) => {
-  return await axios.patch(
+  const res = await axios.patch(
     `${BASE_URL}/api/bookings/${bookingId}/cancel`,
     null,
     {
       headers: { Authorization: `Bearer ${token}` },
     }
   );
+
+  return res.data;
 };
