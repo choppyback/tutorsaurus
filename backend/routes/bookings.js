02ec82ddc8105bc80cb2361781d30bbac342e596
choppyback <seth.tay2812@gmail.com>
2025-07-22 20:33:15 +0800

    refactor: update booking routes to use unified updateBookingStatusController

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -8,16 +8,28 @@ const createBooking = require("../controllers/bookingController");
 const getTutorBookedTimes = require("../controllers/getTutorBookedTimes");
 const studentBookings = require("../controllers/studentBookingsController");
 const tutorBookings = require("../controllers/tutorBookingsController");
-const cancelBooking = require("../controllers/cancelBookingController");
-const confirmBooking = require("../controllers/confirmBookingController");
+const updateBookingStatusController = require("../controllers/updateBookingStatusController");
 
 // Routes
 router.post("/", authenticate, createBooking);
 router.get("/student", authenticate, studentBookings);
 router.get("/tutor", authenticate, tutorBookings);
 router.get("/:id", getTutorBookedTimes);
-router.patch("/:id/cancel", authenticate, cancelBooking);
-router.patch("/:id/confirm", authenticate, confirmBooking);
+router.patch(
+  "/:id/cancel",
+  authenticate,
+  updateBookingStatusController("cancelled")
+);
+router.patch(
+  "/:id/confirm",
+  authenticate,
+  updateBookingStatusController("confirmed")
+);
+router.patch(
+  "/:id/complete",
+  authenticate,
+  updateBookingStatusController("completed")
+);
 //router.delete("/:id", authenticate, deleteBooking);
 
 module.exports = router;
