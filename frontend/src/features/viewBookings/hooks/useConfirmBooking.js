7fd70474d43def0c13750ea0cd6471661308b704
choppyback <seth.tay2812@gmail.com>
2025-07-22 20:38:14 +0800

    refactor: update useConfirmBooking

--- frontend/src/features/viewBookings/hooks/useConfirmBooking.js
+++ frontend/src/features/viewBookings/hooks/useConfirmBooking.js
@@ -1,23 +1,28 @@
+import { useCallback } from "react";
 import axios from "axios";
 import BASE_URL from "../../../config/api";
 
 export const useConfirmBooking = (token, fetchBookings) => {
-  const confirmBooking = async (bookingId) => {
-    try {
-      const res = await axios.patch(
-        `${BASE_URL}/api/bookings/${bookingId}/confirm`,
-        {},
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
+  const confirmBooking = useCallback(
+    async (bookingId) => {
+      try {
+        const res = await axios.patch(
+          `${BASE_URL}/api/bookings/${bookingId}/confirm`,
+          null,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
 
-      alert("Booking confirmed successfully.");
-      fetchBookings();
-    } catch (err) {
-      console.error("Failed to confirm booking", err);
-    }
-  };
+        alert("Booking confirmed successfully.");
+        // Refetch bookings to update new state
+        fetchBookings();
+      } catch (err) {
+        console.error("Failed to confirm booking", err);
+      }
+    },
+    [token, fetchBookings]
+  );
 
   return confirmBooking;
 };
