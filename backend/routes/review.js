8304170f66e41c3e361754f70895d2b66217abcc
choppyback <seth.tay2812@gmail.com>
2025-07-24 12:53:48 +0800

    feat: add viewReview controller and route for fetching review

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -5,8 +5,10 @@ const authenticate = require("../middleware/authenticate");
 
 // Import Controller
 const submitReview = require("../controllers/submitReviewController");
+const viewReview = require("../controllers/viewReviewController");
 
 // Routes
 router.post("/", authenticate, submitReview);
+router.get("/:bookingId", authenticate, viewReview);
 
 module.exports = router;
