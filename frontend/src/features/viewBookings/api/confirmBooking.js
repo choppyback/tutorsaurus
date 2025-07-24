e7f43ef73249ab4ab765706bc16343336bd618b8
choppyback <seth.tay2812@gmail.com>
2025-07-24 16:09:51 +0800

    chore: remove outdated comment

--- frontend/src/features/viewBookings/api/confirmBooking.js
+++ frontend/src/features/viewBookings/api/confirmBooking.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import BASE_URL from "../../../config/api";
 
-// change booking status to cancelled
-
 export const confirmBooking = async (bookingId, token) => {
   const res = await axios.patch(
     `${BASE_URL}/api/bookings/${bookingId}/confirm`,
